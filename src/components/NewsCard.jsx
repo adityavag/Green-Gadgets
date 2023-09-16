c6b7f2ba2a1d0903fcd76306d70afccda4aac574
Ankit-Patra <ankitpatra0123@gmail.com>
2023-09-16 14:34:34 +0530

    fix/Responsiveness issue of cards

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NewsCard = (props) => {
     return (
-        <a href={props.link}>
-            <div className="block max-w-full m-4 p-6 border border-gray-200 rounded-lg shadow  mb-8 bg-[#014558]">
+        <a href={props.link} className='my-4'>
+            <div className="block max-w-full min-h-full mx-4 p-6 border border-gray-200 rounded-lg shadow  mb-8 bg-[#014558]">
                 <h5 className="mb-2 text-2xl text-white font-semibold tracking-tight">{props.title}</h5>
                 <p className="font-normal text-white">{props.description}</p>
             </div>
