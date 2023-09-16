360fd55a41e8c4cefd1944dd603f232405f4bcc3
adi247u <adi247u@gmail.com>
2023-09-16 06:07:28 +0530

    Second Commit

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 
-const NewsCard = ({ article }) => {
+const NewsCard = (props) => {
     return (
-        // <div>
-        //     <div className="p-8">
-        //         <a href={article.url} target="_blank" rel="noopener noreferrer" className=''>
-        //             <img src={article.urlToImage} alt={article.title} />
-        //             <h3>{article.title}</h3>
-        //         </a>
-        //         <p>{article.description}</p>
-        //     </div>
-        // </div>
         <div>
-            <a href={article.url} className="block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-100" target="_blank">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight">{article.title}</h5>
-                <img src={article.urlToImage} alt={article.title} />
-                <p className="font-normal text-gray-700">{article.description}</p>
-            </a>
+            <div className="block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-100 mb-8">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight">{props.title}</h5>
+                <p className="font-normal text-gray-700">{props.description}</p>
+            </div>
         </div>
     );
 };
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,62 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import NewsCard from '../components/NewsCard'; // Import the NewsCard component
-import Navbar from '../components/Navbar';
-function News() {
-  const [news, setNews] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [currentPage, setCurrentPage] = useState(1);
-  const articlesPerPage = 5;
-
-  useEffect(() => {
-    const apiUrl =
-      'https://newsapi.org/v2/everything?q=e-waste&from=2023-09-14&to=2023-09-14&sortBy=popularity&apiKey=d08dfa0f47224f17bc951ab78b1ae925';
-
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.articles) {
-
-          setNews(data.articles);
-          setLoading(false);
-        } else {
-          console.error('Data structure is not as expected:', data);
-          setLoading(false);
-        }
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-        setLoading(false);
-      });
-  }, []);
-
-  const indexOfLastArticle = currentPage * articlesPerPage;
-  const indexOfFirstArticle = indexOfLastArticle - articlesPerPage;
-  const currentArticles = news.slice(indexOfFirstArticle, indexOfLastArticle);
-
-
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+import React, { useEffect, useState } from 'react'
+import NewsCard from '../components/NewsCard'
+import newsData from '../data/articles.json'
+import Navbar from '../components/Navbar'
+export default function News() {
+  const newsArticles = newsData.map(news => {
+    return <NewsCard title={news.title} description={news.description} content={news.content} date={news.publishedAt} />
+  })
 
   return (
     <div>
       <Navbar />
-      <div className=''></div>
-      <div className='p-8'>
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          <div className=''>
-            <ul className='grid grid-cols-3 max-lg:grid-cols-1 max-lg:mx-auto'>
-              {currentArticles.map((item, index) => (
-                <li key={index} className='max-lg:mt-4'>
-                  <NewsCard article={item} />
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
+      <div className='p-8 grid grid-cols-3 max-lg:grid-cols-1'>
+        {newsArticles}
       </div>
     </div>
-  );
+  )
 }
-
-export default News;
